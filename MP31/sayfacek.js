f9f8e8d086c01096666a0d525beb64def9277416
Ahmet Burçoğlu <ahmetburcoglu2@gmail.com>
2018-08-11 18:47:37 +0300

    Update sayfacek.js

--- MP31/sayfacek.js
+++ MP31/sayfacek.js
@@ -3,9 +3,9 @@ chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}
    function(tabs){
 	   if (tabs[0].url.split("=")[0].includes("youtube.com")){
 		   var gidecek = tabs[0].url.split("=")[1];
-		   var yol='<iframe class="ytmp3_api" src="https://ytmp3.me/@api/mp3/'+gidecek+'" width="100%" height="100%" allowtransparency="true" scrolling="no" style="border:none;"></iframe>';
-	   document.getElementById("sayfaid").innerHTML = yol;
-	  }
+ 		 var yol2='<iframe width="250px" height="60px" scrolling="no" style="border:none;" src="https://www.download-mp3-youtube.com/api/?api_key=MTczMTAyMTQ2&format=mp3&video_id='+gidecek+'"></iframe>';
+  		 document.getElementById("sayfaid").innerHTML = yol2;
+  }
    }
 );
 		
